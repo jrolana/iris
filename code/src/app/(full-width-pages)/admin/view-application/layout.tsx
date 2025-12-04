33bd6e7bafdd4ccf521bda1275cad4d07ab1b35c
Andry Tumacole <ajtajaja13@gmail.com>
2025-12-04 21:20:09 +0800

    fix: moved the view application pages to have full width

---
+++ code/src/app/(full-width-pages)/admin/view-application/layout.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import React from "react";
+import { useRole } from '@/hooks/useRole'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const { isAuthorized, isLoading } = useRole(['admin'])
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (!isAuthorized) {
+    return <div>You are not authorized to access this page.</div>
+  }
+  return <div>{children}</div>;
+}
--- code/src/app/(homepage)/layout.tsx
+++ code/src/app/(homepage)/layout.tsx
@@ -1,19 +1,18 @@
 'use client'
 
 import React from "react";
-// import BaseLayout from "@/layout/BaseLayout";
-// import { NavItem } from "@/lib/types/nav";
-// import { GridIcon, TableIcon, DocsIcon, DownloadIcon } from "@/icons";
+import BaseLayout from "@/layout/BaseLayout";
+import { NavItem } from "@/lib/types/nav";
+import { GridIcon, TableIcon, DocsIcon, DownloadIcon } from "@/icons";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 
-  // const navItems: NavItem[] = [
-  //   { icon: <GridIcon />, name: "Dashboard", path: "/" },
-  //   { icon: <TableIcon />, name: "Applications Registry", path: "/" },
-  //   { icon: <DocsIcon />, name: "Application Guide", path: "/" },
-  //   { icon: <DownloadIcon />, name: "Application Documents", path: "/" },
-  // ];
+  const navItems: NavItem[] = [
+    { icon: <GridIcon />, name: "Dashboard", path: "/" },
+    { icon: <TableIcon />, name: "Applications Registry", path: "/" },
+    { icon: <DocsIcon />, name: "Application Guide", path: "/" },
+    { icon: <DownloadIcon />, name: "Application Documents", path: "/" },
+  ];
 
-  // return <BaseLayout navItems={navItems} isPublic={true}>{children}</BaseLayout>;
-  return <div>{children}</div>
+  return <BaseLayout navItems={navItems} isPublic={true}>{children}</BaseLayout>;
 }
