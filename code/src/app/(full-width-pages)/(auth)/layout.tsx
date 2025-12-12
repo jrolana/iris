aad716890de245cee73369b03b04132e6f72aed3
jrolana <olanajhoanna@gmail.com>
2025-12-12 22:17:38 +0800

    [Olana][Enhancement] Sigin In Page

--- code/src/app/(full-width-pages)/(auth)/layout.tsx
+++ code/src/app/(full-width-pages)/(auth)/layout.tsx
@@ -1,7 +1,6 @@
-import GridShape from "@/components/common/GridShape";
-
 // import Image from "next/image";
 import Link from "next/link";
+import { ChevronLeftIcon } from "@/icons";
 import React from "react";
 
 export default function AuthLayout({
@@ -10,27 +9,37 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative z-1 bg-white p-6 sm:p-0 dark:bg-gray-900">
-      <div className="relative flex h-screen w-full flex-col justify-center sm:p-0 lg:flex-row dark:bg-gray-900">
-        {children}
-        <div className="bg-brand-950 hidden h-full w-full items-center lg:grid lg:w-1/2 dark:bg-white/5">
+    <div className="relative z-1 bg-white p-6 sm:p-0">
+      <div className="items-centersm:p-0 relative flex h-screen w-full flex-col justify-center lg:flex-row">
+        <div className="no-scrollbar flex w-full flex-1 flex-col overflow-y-auto lg:w-1/2">
+          <div className="mx-auto w-full max-w-md sm:pt-10">
+            <Link
+              href="/"
+              className="inline-flex items-center text-sm text-gray-500 transition-colors hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+            >
+              <ChevronLeftIcon />
+              Back to dashboard
+            </Link>
+          </div>
+          {children}
+        </div>
+
+        <div className="bg-brand-950 hidden h-full w-full items-center lg:grid lg:w-1/2">
           <div className="relative z-1 flex items-center justify-center">
-            {/* <!-- ===== Common Grid Shape Start ===== --> */}
-            <GridShape />
-            {/* <div className="flex flex-col items-center max-w-xs">
-                <Link href="/" className="block mb-4">
-                  <Image
-                    width={231}
-                    height={48}
-                    src="./images/logo/auth-logo.svg"
-                    alt="Logo"
-                  />
-                </Link>
-                <p className="text-center text-gray-400 dark:text-white/60">
-                  Free and Open-Source Tailwind CSS Admin Dashboard Template
-                </p>
-              </div> */}
-            <div className="flex max-w-xs flex-col items-center text-white/90">
+            {/* <div className="flex max-w-xs flex-col items-center">
+              <Link href="/" className="mb-4 block">
+                <Image
+                  width={231}
+                  height={48}
+                  src="images/logo/auth-logo.svg"
+                  alt="Logo"
+                />
+              </Link>
+              <p className="text-center text-gray-400 dark:text-white/60">
+                Free and Open-Source Tailwind CSS Admin Dashboard Template
+              </p>
+            </div> */}
+            <div className="mt-15 flex flex-col items-center text-white/90">
               <Link href="/" className="mb-4 block text-center">
                 <span className="text-5xl font-extrabold tracking-[0.35em] text-white">
                   IRIS
@@ -40,7 +49,7 @@ export default function AuthLayout({
                 </div>
               </Link>
 
-              <p className="text-center text-white/80">
+              <p className="text-center text-white/80 w-[60%]">
                 Intellectual Property Rights Management System for the
                 University of the Philippines Visayas
               </p>
