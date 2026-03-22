5b7691764cbbdd20b6212accd2dc79bf98f0710c
jrolana <olanajhoanna@gmail.com>
2026-03-22 14:43:04 +0800

    [Olana][Enhancement] View Better Ref List

--- code/scripts/create_views.sql
+++ code/scripts/create_views.sql
@@ -1,80 +1,64 @@
 DROP VIEW IF EXISTS public.v_dashboard_status CASCADE;
+
+
 CREATE VIEW public.v_dashboard_status
--- secuirty_barrier for protection
+-- security_barrier for protection
 -- disabled security_invoker so that this uses the
 -- view's creator's privileges (security_definer)
 -- hence even when anon have limited rls policy on the underlying tables of this view,
 -- it can still see this view
 WITH (security_barrier = true)
 AS
+WITH status_class AS (
+    SELECT 
+    a.*,
+    CASE
+      WHEN a.is_withdrawn = true THEN 'withdrawn'
+      WHEN s.status_type = 'downgraded_to_um' THEN 'downgraded'
+      WHEN s.status_type = 'registered' THEN 'granted'
+      WHEN s.status_type IN (
+        'filed_with_ipophil',
+        'wait_registrability_report',
+        'resolve_rr_defects',
+        'wait_formality_exam_report',
+        'resolve_fer_defects',
+        'request_revival',
+        'wait_notice_publication',
+        'published',
+        'request_substantive_exam_report',
+        'wait_substantive_exam_report',
+        'resolve_ser_defects',
+        'wait_notice_of_issuance',
+        'req_cert_of_registration',
+        'wait_cert_of_registration',
+        'req_for_issuance_of_cert_and_2nd_publication',
+        '2nd_publication',
+        'wait_notice_of_action',
+        'resolve_additional_requirements',
+        'mailed_to_ipophl',
+        'wait_statement_of_acc',
+        'pay_fee_application',
+        'prepare_nice_classification',
+        'approve_nice_classification'
+      ) THEN 'filed'
+      ELSE 'pending'
+    END AS dashboard_status
+  FROM private.ipr_applications AS a
+  JOIN private.ipr_statuses AS s
+    ON s.id = a.curr_status
+)
 SELECT 
-  a.id,
-  a.ip_type,
-  CASE
-    WHEN a.is_withdrawn = true THEN 'withdrawn'
-    WHEN s.status_type = 'downgraded_to_um' THEN 'downgraded'
-    WHEN s.status_type = 'registered' THEN 'granted'
-    WHEN s.status_type IN (
-      'filed_with_ipophil',
-      'wait_registrability_report',
-      'resolve_rr_defects',
-      'wait_formality_exam_report',
-      'resolve_fer_defects',
-      'request_revival',
-      'wait_notice_publication',
-      'published',
-      'request_substantive_exam_report',
-      'wait_substantive_exam_report',
-      'resolve_ser_defects',
-      'wait_notice_of_issuance',
-      'req_cert_of_registration',
-      'wait_cert_of_registration',
-      'req_for_issuance_of_cert_and_2nd_publication',
-      '2nd_publication',
-      'wait_notice_of_action',
-      'resolve_additional_requirements',
-      'mailed_to_ipophl',
-      'wait_statement_of_acc',
-      'pay_fee_application',
-      'prepare_nice_classification',
-      'approve_nice_classification'
-    ) THEN 'filed'
-    ELSE 'pending'
-  END AS dashboard_status,
-  CASE
-    WHEN a.is_withdrawn = true THEN a.updated_at
-    WHEN s.status_type = 'downgraded_to_um' THEN a.updated_at
-    WHEN s.status_type = 'registered' THEN a.registration_date::timestamptz
-    WHEN s.status_type IN (
-      'filed_with_ipophil',
-      'wait_registrability_report',
-      'resolve_rr_defects',
-      'wait_formality_exam_report',
-      'resolve_fer_defects',
-      'request_revival',
-      'wait_notice_publication',
-      'published',
-      'request_substantive_exam_report',
-      'wait_substantive_exam_report',
-      'resolve_ser_defects',
-      'wait_notice_of_issuance',
-      'req_cert_of_registration',
-      'wait_cert_of_registration',
-      'req_for_issuance_of_cert_and_2nd_publication',
-      '2nd_publication',
-      'wait_notice_of_action',
-      'resolve_additional_requirements',
-      'mailed_to_ipophl',
-      'wait_statement_of_acc',
-      'pay_fee_application',
-      'prepare_nice_classification',
-      'approve_nice_classification'
-    ) THEN a.filing_date::timestamptz
-    ELSE a.created_at
+  id,
+  ip_type,
+  dashboard_status,
+  CASE dashboard_status
+    WHEN 'withdrawn'   THEN updated_at
+    WHEN 'downgraded'  THEN updated_at
+    WHEN 'granted'     THEN registration_date::timestamptz
+    WHEN 'filed'       THEN filing_date::timestamptz
+    ELSE created_at
   END AS time_concerned
-FROM private.ipr_applications AS a
-JOIN private.ipr_statuses AS s
-  ON s.id = a.curr_status;
+FROM status_class;
 
 DROP VIEW IF EXISTS public.v_dashboard_analytics;
 CREATE VIEW public.v_dashboard_analytics
