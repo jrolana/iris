84c1b2b0d1d9b1e5ac3bb7b718a7783b7ff5ea7a
jrolana <olanajhoanna@gmail.com>
2026-04-20 21:54:38 +0800

    [Olana][Enhancement] Used previous welcome page link to redirect users upon accepting invite, used prev email template, Irrelevant format chnange was caused by prettier

--- code/src/app/actions/invite-user.ts
+++ code/src/app/actions/invite-user.ts
@@ -1,49 +1,47 @@
-"use server"
+"use server";
 
 import { RegistrationRequestType } from "@/lib/types/users";
 import { supabaseAdmin } from "../../../utils/supabase/admin";
 
 interface PropsInterface {
-    userData: RegistrationRequestType["Update"];
-    email: string;
+  userData: RegistrationRequestType["Update"];
+  email: string;
 }
 
 export async function inviteUser(props: PropsInterface) {
-    const {email, userData} = props;
+  const { email, userData } = props;
 
-    const url = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"  ;
+  const url = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
-    const { data, error } = await supabaseAdmin.auth.admin.generateLink({
-        type: 'invite',
+  const { data, error } = await supabaseAdmin.auth.admin.generateLink({
+    type: "invite",
+    email: email,
+    options: {
+      data: userData,
+    },
+  });
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  const inviteLink = `${url}/signin/callback?token_hash=${encodeURIComponent(data.properties.hashed_token)}&type=invite`;
+
+  const {full_name, role} = userData;
+
+  const { data: emailData, error: emailError } =
+    await supabaseAdmin.functions.invoke("send-invite-email", {
+      body: {
         email: email,
-        options: {
-        data: userData,
-        redirectTo: `${url}/invite`
-        }
+        inviteLink: inviteLink,
+        fullName: full_name, 
+        role: role 
+      },
     });
 
-    if (error) {
-        throw new Error(error.message);
-    }
+  if (emailError) {
+    throw new Error(emailError.message);
+  }
 
-    const inviteLink = data.properties.action_link;
-
-
-    const {full_name, role} = userData;
-
-
-    const { data: emailData, error: emailError } = await supabaseAdmin.functions.invoke('send-invite-email', {
-        body: {
-            email: email,
-            inviteLink: inviteLink,
-            fullName: full_name, 
-            role: role 
-        }
-    });
-
-    if (emailError) {
-        throw new Error (emailError.message)
-    } 
-
-    return emailData;
+  return emailData;
 }
