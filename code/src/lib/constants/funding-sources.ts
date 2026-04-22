b91fa468ef9e000a06d4682a69df46ebbbe2016f
Andry Tumacole <ajtajaja13@gmail.com>
2026-04-22 13:54:44 +0800

    feat(start application): funding source is now required and has a dropdown of common funding sources by ttbdo. the user can also choose to provide other (maybe private) funding sources as well

---
+++ code/src/lib/constants/funding-sources.ts
@@ -0,0 +1,12 @@
+export const FUNDING_SOURCE_OPTIONS = [
+  "UP System",
+  "UPV In-house Budget",
+  "DOST-PCAARRD",
+  "DOST-PCHRD",
+  "DOST-PCIEERRD",
+  "DOST-TAPI",
+  "BFAR",
+  "DA",
+  "Not applicable",
+  "Others",
+] as const;
